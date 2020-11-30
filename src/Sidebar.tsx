3b6a23c132c8655db4210d83e970981a34b8f208
abuhorainhero <abuhorain2016@gmail.com>
2020-12-01 02:33:37 +0600

    sidebar average complete

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -4,6 +4,8 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 const Sidebar = () => {
     return (
@@ -21,6 +23,19 @@ const Sidebar = () => {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+            </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     );
